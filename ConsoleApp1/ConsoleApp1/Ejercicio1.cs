dc1a6e173b644ed70ef663366d543f5f0a876ecd
santiarroyave <santi98cv@hotmail.com>
2023-08-09 16:48:37 +0200

    Añade ejercicios

---
+++ ConsoleApp1/ConsoleApp1/Ejercicio1.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); // Para la ejecución hasta presionar tecla
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Ejercicio2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("HOLA MUNDO!!!");
+            int num1 = 5;
+            int num2 = 6;
+            double num3 = 2.57;
+            double suma;
+
+            suma = num1 + num3;
+            Console.WriteLine(suma);
+
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El número introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+    }
+}
